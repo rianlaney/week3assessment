6e902d7598d93c38b6cf7cfa57ec14660f7f1d0b
Rian Laney <rianlaney@gmail.com>
2023-04-07 12:28:20 -0400

    fixed profile.js

--- profile.js
+++ profile.js
@@ -3,7 +3,7 @@ let color = document.getElementById('color')
 color.addEventListener('click', (event)=> alert('green'))
 
 let place = document.getElementById('place')
-place.addEventListener('click', (event)=> alert('green'))
+place.addEventListener('click', (event)=> alert('st pete, duh'))
 
 let ritual = document.getElementById('ritual')
-ritual.addEventListener('click', (event)=> alert('green'))
+ritual.addEventListener('click', (event)=> alert('walking the doggo'))
